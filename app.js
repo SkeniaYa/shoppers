89f0912db8d029d5a97b243ec2050fd32963fdad
Kseniya <yabuzarovakm@gmail.com>
2021-06-03 14:49:51 +0300

    edit hbs

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-const express = require('express');
-const path = require('path')
-const morgan = require('morgan')
-const hbs = require('hbs')
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const hbs = require("hbs");
+const indexRouter = require("./routes/indexRouter");
 
-const PORT = 3000 
-const app = express()
+const PORT = 3000;
+const app = express();
 
-
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'views'))
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
-
-
-// подключение routers
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
 
 // подключение routers
+app.get("/", indexRouter);
 
+// подключение routers
 
 // Start middleware section
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 // Start middleware section
 
 app.listen(PORT, () => {
-    console.log('server work >>>', PORT)
-})
-
-
+  console.log("server work >>>", PORT);
+});
