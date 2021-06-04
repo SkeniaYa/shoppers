442886792cd072b053019a341ad6be240bb9f605
Milyukovich <milyukovartur@gmail.com>
2021-06-04 15:28:59 +0300

    v 1.9

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,13 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
-// const constructor = document.querySelector("#constructor");
 const container = document.querySelector("#container");
 const select = document.querySelector("#selectColor");
 const imgContainer = document.querySelector(".wrapper-images");
 const mainImg = document.querySelector("#mainImg");
+const downloadImgForm = document.forms.downloadImg;
+const addTextButton = document.querySelector("#addText");
+const containerAddConva = document.querySelector("#containerAddConva");
+const inputText = document.querySelector("#addTxt");
 
 const stage = new Konva.Stage({
   container: "container",
@@ -15,95 +18,132 @@ const stage = new Konva.Stage({
 const layer = new Konva.Layer();
 stage.add(layer);
 
-// constructor.addEventListener("click", (e) => {
-//   if (e.target.className === "delete") {
-//   }
-//   if (e.target.className === "addtext") {
-//     const text1 = new Konva.Text({
-//       x: 10,
-//       y: 70,
-//       fontSize: 30,
-//       text: "Best app in the World",
-//       draggable: true,
-//     });
-//     layer.add(text1);
-
-//     const tr1 = new Konva.Transformer({
-//       nodes: [text1],
-//       keepRatio: true,
-//       enabledAnchors: ["top-left", "top-right", "bottom-left", "bottom-right"],
-//     });
-//     layer.add(tr1);
-//   }
-//   if (e.target.className === "addimg") {
-//     function drawImage(imageObj) {
-//       var darthVaderImg = new Konva.Image({
-//         image: imageObj,
-//         x: stage.width() / 2 - 600,
-//         y: stage.height() / 2 - 330,
-//         // x: stage.width() / 2 - 200 / 2,
-//         // y: stage.height() / 2 - 137 / 2,
-//         width: 200,
-//         height: 137,
-//         draggable: true,
-//       });
-
-//       // add cursor styling
-//       darthVaderImg.on("mouseover", function () {
-//         document.body.style.cursor = "pointer";
-//       });
-//       darthVaderImg.on("mouseout", function () {
-//         document.body.style.cursor = "default";
-//       });
-
-//       layer.add(darthVaderImg);
-//       stage.add(layer);
-
-//       const tr2 = new Konva.Transformer({
-//         nodes: [darthVaderImg],
-//         keepRatio: true,
-//         enabledAnchors: [
-//           "top-left",
-//           "top-right",
-//           "bottom-left",
-//           "bottom-right",
-//         ],
-//       });
-//       layer.add(tr2);
-//     }
-//     const imageObj = new Image();
-//     imageObj.onload = function () {
-//       drawImage(this);
-//     };
-//     imageObj.src = "/";
-//   }
-// });
-
 select.addEventListener("change", async (event) => {
   const color = event.target.value;
-  const collection = "BagColor";
-  const modelName = "model 1";
-
+  // const collection = "BagColor";
+  const modelName = mainImg.firstChild.className;
+  // console.log(modelName);
   const response = await fetch("/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ color, collection, modelName }),
+    body: JSON.stringify({ color, modelName }),
   });
 
   const newItem = await response.json();
+  // console.log(newItem);
+  const img = document.createElement("img");
+  img.src = newItem.image;
+  img.classList.add(modelName);
+  // console.log(img);
+
+  mainImg.innerText = "";
+  mainImg.appendChild(img);
+  mainImg.insertAdjacentHTML(
+    "beforeend",
+    `<div class="forUploading">
+      <div id="container"></div>
+    </div>`
+  );
 });
 
 imgContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "IMG") {
+    // console.log(e.target);
     mainImg.innerText = "";
     const html = document.createElement("img");
     html.src = e.target.src;
+    html.classList.add(e.target.className);
+    // console.log(e.target.className);
     mainImg.appendChild(html);
+    mainImg.insertAdjacentHTML(
+      "beforeend",
+      `<div class="forUploading">
+        <div id="container"></div>
+      </div>`
+    );
   }
 });
 
+downloadImgForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData();
+  formData.append("file", e.target.image.files[0]);
+  console.log(e.target.image.files[0]);
+  const response = await fetch("/", {
+    method: "POST",
+    body: formData,
+  });
+  e.target.image.value = "";
+  // downloadImgForm.reset();
+  const newItem = await response.json();
+  // console.log(newItem.image);
+  if (newItem) {
+    function drawImage(imageObj) {
+      var darthVaderImg = new Konva.Image({
+        image: imageObj,
+        x: stage.width() / 2 - 640,
+        y: stage.height() / 2 - 370,
+        // x: stage.width() / 2 - 200 / 2,
+        // y: stage.height() / 2 - 137 / 2,
+        width: 120,
+        height: 87,
+        draggable: true,
+      });
+
+      // add cursor styling
+      darthVaderImg.on("mouseover", function () {
+        document.body.style.cursor = "pointer";
+      });
+      darthVaderImg.on("mouseout", function () {
+        document.body.style.cursor = "default";
+      });
+
+      layer.add(darthVaderImg);
+      stage.add(layer);
+
+      const tr2 = new Konva.Transformer({
+        nodes: [darthVaderImg],
+        keepRatio: true,
+        enabledAnchors: [
+          "top-left",
+          "top-right",
+          "bottom-left",
+          "bottom-right",
+        ],
+      });
+      layer.add(tr2);
+    }
+    const imageObj = new Image();
+    imageObj.onload = function () {
+      drawImage(this);
+    };
+    imageObj.src = `/downloadImg/${newItem.image}`;
+  }
+});
+
+containerAddConva.addEventListener("click", async (e) => {
+  if (e.target.className === "addText") {
+    const text1 = new Konva.Text({
+      x: 10,
+      y: 70,
+      fontSize: 30,
+      text: `${inputText.value}`,
+      draggable: true,
+    });
+    layer.add(text1);
+
+    const tr1 = new Konva.Transformer({
+      nodes: [text1],
+      keepRatio: true,
+      enabledAnchors: ["top-left", "top-right", "bottom-left", "bottom-right"],
+    });
+    layer.add(tr1);
+  }
+  inputText.value = "";
+});
 // container.addEventListener("click", (e) => {
 //   console.log(e.target);
 // });
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,18 +6,18 @@ const BagSize = require("../models/bagSize");
 const Material = require("../models/material");
 
 router.get("/", async (req, res) => {
-  console.log(11111111111);
+  // console.log(11111111111);
   const sizesFromDB = await BagSize.find();
   const colorsFromDB = await BagColor.find();
   const materialsFromDB = await Material.find();
-  console.log("--->>>", sizesFromDB);
+  // console.log("--->>>", sizesFromDB);
   const uniqueSizes = [];
   for (let i = 0; i < sizesFromDB.length; i++) {
     if (!uniqueSizes.includes(sizesFromDB[i].sizeName)) {
       uniqueSizes.push(sizesFromDB[i].sizeName);
     }
   }
-  console.log("unique-->>>", uniqueSizes);
+  // console.log("unique-->>>", uniqueSizes);
 
   const uniqueColors = [];
   for (let i = 0; i < colorsFromDB.length; i++) {
@@ -25,38 +25,42 @@ router.get("/", async (req, res) => {
       uniqueColors.push(colorsFromDB[i].color);
     }
   }
-  console.log("uniqueColors", uniqueColors);
+  // console.log("uniqueColors", uniqueColors);
   res.render("index", { uniqueSizes, uniqueColors, materialsFromDB });
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  // if (req.body) {
+  //   const { color, modelName } = req.body;
+  //   console.log("modelName", modelName);
+  //   console.log("color", color);
+  //   const test = await BagColor.findOne({ color, title: modelName });
+  //   console.log("test", test);
+  //   res.json(test);
+  // }
+  // console.log(req.files);
+  console.log(12);
+  console.log("req body ====> ", req.files);
   if (req.files) {
-    const { image } = req.files;
-
+    const { file } = req.files;
+    console.log(file.name);
     const path =
-      __dirname.replace("routes", "") + `/public/downloadImg/${image.name}`;
-    image.mv(path, (err) => {
-      return res.render("index", { image: image.name });
+      __dirname.replace("routes", "") + `/public/downloadImg/${file.name}`;
+    file.mv(path, (err) => {
+      res.json({ image: file.name });
     });
-  } else {
-    res.render("index");
   }
 });
 
-// router.post("/", async (req, res) => {
-//   // const { color, collection, modelName } = req.body;
-//   console.log(req.body);
-//   // const test = await collection.find({ title: modelName, color });
-//   // console.log(test);
-// });
+// router.post("/", async (req, res) => {});
 // router.get("/help", (req, res) => {
 //   res.render("help");
 // });
 
 module.exports = router;
-router.get("/thankyou", (req, res) => {
-  res.render("done");
-});
-router.get("/helper", (req, res) => {
-  res.render("help");
-});
+// router.get("/thankyou", (req, res) => {
+//   res.render("done");
+// });
+// router.get("/helper", (req, res) => {
+//   res.render("help");
+// });
